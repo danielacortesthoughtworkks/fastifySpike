a0c41c524807c9f9c8aa50c0c581dc2812b2d395
Gabriel Villacis <gvillaci@thoughtworks.com>
2020-04-17 10:46:05 -0400

    configure ES6

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import helloRoute from './src/route/helloRoute';
+
+const fastify = require('fastify')({
+  logger: true
+});
+
+const start = async () => {
+  try {
+      fastify.register(helloRoute);
+      await fastify.listen(3000);
+      fastify.log.info(`Server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+      fastify.log.error(err);
+      process.exit(1);
+  }
+}
+
+start();
